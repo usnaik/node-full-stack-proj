f3a4cc14ea78746a1bedab228517877895cd7fa1
usnaik <upendra_naik@yahoo.com>
2020-05-17 12:52:23 +0530

    Initial author routes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	// res.send('Hello world');
-	res.render('index');
+	res.render('index'); // displays layout  with 	<%- body %>	tag is replaces with this file in views.
 });
 
 module.exports = router;
